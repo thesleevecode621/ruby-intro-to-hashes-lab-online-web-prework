d5e7198ec5ef3303c2a556a551f9b7db73287368
boruch d <dubywider@gmail.com>
2019-04-10 18:53:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,10 +20,7 @@ end
 def monopoly_with_second_tier
 
 		monopoly = {:railroads => {:pieces => 4,
-	:names => {:reading_railroad => {},
-						 :pennsylvania_railroad => {},
-						 :b_and_o_railroad => {},
-						 :shortline => {}},
+	:names => {:},
 	:rent_in_dollars => {}
 	}
 }
